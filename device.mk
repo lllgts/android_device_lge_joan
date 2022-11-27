c5c4f9bcf82072fcbed406e3d1361178b201200e
Bruno Martins <bgcngm@gmail.com>
2022-11-27 09:32:42 +0700

    joan: Migrate to AIDL ClearKey DRM HAL

--- device.mk
+++ device.mk
@@ -167,8 +167,9 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
-    android.hardware.drm@1.1.vendor
+    android.hardware.drm-service.clearkey \
+    android.hardware.drm@1.4 \
+    android.hardware.drm@1.4.vendor
 
 #Filesystem management tools
 PRODUCT_PACKAGES += \
